a2891904bfc740c04eef08a715cef29d0b839c4d
hzethraeus <hzethraeus@gmail.com>
2022-06-17 22:11:25 +0200

    modal test

--- eigen/pages/test1.js
+++ eigen/pages/test1.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 import styles from "./test1.module.css";
+import Modal from "../components/modal";
 
 export default function test1() {
   const [flip, setFlip] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   console.log(flip);
+
   return (
-    <div
-      onClick={() => setFlip(!flip)}
-      className={`${styles.card} ${flip ? `${styles.flip}` : ""}`}
-    >
-      <div className={styles.front}>FRONT</div>
-      <div className={styles.back}>BACK</div>
-    </div>
+    <>
+      <div
+        onClick={() => setFlip(!flip)}
+        className={`${styles.card} ${flip ? `${styles.flip}` : ""}`}
+      >
+        <div className={styles.front}>FRONT</div>
+        <div className={styles.back}>BACK</div>
+      </div>
+      <div onClick={() => setShowModal(true)}>Open Modal</div>
+      <Modal show={showModal} onClose={() => setShowModal(false)} />
+    </>
   );
 }
